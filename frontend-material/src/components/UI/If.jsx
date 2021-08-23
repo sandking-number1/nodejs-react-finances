966a527def644dfa448cbffb54ac0f8a130a4d93
sandking-number1 <Alfred.J9412@gmail.com>
2021-08-22 23:19:30 -0300

    config app directories

--- frontend-material/src/components/UI/If.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const If = props => {
-	if (!props.show) return '';
-	return <React.Fragment>{props.children}</React.Fragment>;
-};
-
-export default If;
--- frontend-material/src/index.js
+++
@@ -1,8 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-import 'fontsource-roboto';
-
-ReactDOM.render(<App />, document.getElementById('root'));
